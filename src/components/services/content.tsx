0a3e4ef09e59ff63902ec3e4657da0074480daa1
risan <potallegta@gmail.com>
2025-12-18 10:28:20 +0700

    feat: service content

--- src/components/services/content.tsx
+++ src/components/services/content.tsx
@@ -1,9 +1,12 @@
 import { useTranslation } from "react-i18next";
 
-import excellencesIcon1 from "../../assets/home/excellences-icon1.svg";
-import excellencesIcon2 from "../../assets/home/excellences-icon2.svg";
-import excellencesIcon3 from "../../assets/home/excellences-icon3.svg";
-import excellencesMainImg from "../../assets/home/excellences-main.webp";
+import proof1Img from "../../assets/home/proof-1.webp";
+import proof2Img from "../../assets/home/proof-2.webp";
+import proof3Img from "../../assets/home/proof-3.webp";
+
+import proof1Popup from "../../assets/home/proof1-popup.webp";
+import proof2Popup from "../../assets/home/proof2-popup.webp";
+import proof3Popup from "../../assets/home/proof3-popup.webp";
 
 import content1Img from "../../assets/services/content-1.webp";
 import content2Img from "../../assets/services/content-2.webp";
@@ -12,15 +15,47 @@ import content3Img from "../../assets/services/content-3.webp";
 import bgServicesContent from "../../assets/services/bg-services-content.svg";
 
 import "../../i18n";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
+
+const proofImages = [
+  {
+    imgSrc: proof1Img,
+    popupSrc: proof1Popup,
+  },
+  {
+    imgSrc: proof2Img,
+    popupSrc: proof2Popup,
+  },
+  {
+    imgSrc: proof3Img,
+    popupSrc: proof3Popup,
+  },
+];
 
 export default function ServiceContent() {
   const { t } = useTranslation();
+  const [modalImg, setModalImg] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (modalImg) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [modalImg]);
 
   return (
     <div
       className="flex flex-col justify-center items-center bg-cover bg-center border border-purple-400/50 border-t-2"
       style={{ backgroundImage: `url(${bgServicesContent.src})` }}
     >
+      {/* List of Services */}
       <section className="flex flex-col items-start gap-32 px-6 lg:px-[100px] py-12 lg:py-24 text-white leading-relaxed">
         <div className="gap-7 lg:gap-28 grid lg:grid-cols-2">
           <img src={content1Img.src} />
@@ -102,6 +137,75 @@ export default function ServiceContent() {
             </div>
           </div>
         </div>
+      </section>
+
+      {/* Portfolio Proofs */}
+      <section className="relative lg:flex-row gap-28 grid mx-auto lg:my-[4.7rem] mt-[3rem] container">
+        <h2 className="font-semibold text-[2.5rem] text-white lg:text-5xl text-center">
+          {t("serviceProofs.h2")}
+        </h2>
+
+        <Swiper
+          modules={[Pagination, Navigation, Autoplay]}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            640: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3, // lg and above
+            },
+          }}
+          pagination={{
+            el: ".service-pagination",
+            clickable: true,
+          }}
+          navigation={{
+            nextEl: ".service-nav-next",
+            prevEl: ".service-nav-prev",
+          }}
+          spaceBetween={27}
+          className="lg:grid-cols-3 max-w-full"
+        >
+          {proofImages.map(({ imgSrc, popupSrc }, index) => (
+            <SwiperSlide
+              key={index}
+              className="hover:opacity-80 cursor-pointer"
+            >
+              <img
+                src={imgSrc.src}
+                onClick={() => setModalImg(popupSrc.src)}
+                className="rounded-lg"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <div className="flex justify-center service-pagination"></div>
+
+        {modalImg && (
+          <div
+            className="z-[9999] fixed inset-0 flex justify-center items-center bg-black/80 p-6"
+            onClick={() => setModalImg(null)}
+          >
+            {/* Close button */}
+            <button
+              className="top-6 lg:top-6 right-6 lg:right-12 z-[10000] absolute bg-hero px-2 rounded-full font-bold text-white text-3xl"
+              onClick={() => setModalImg(null)}
+            >
+              &times;
+            </button>
+
+            <div
+              className="max-h-full overflow-y-auto"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <img src={modalImg} className="w-auto max-w-full h-auto" />
+            </div>
+          </div>
+        )}
       </section>
     </div>
   );
