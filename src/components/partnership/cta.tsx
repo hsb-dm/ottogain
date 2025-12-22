e6b5914deabf3674d45858e070ebc51d874c67da
risan <potallegta@gmail.com>
2025-12-22 13:58:33 +0700

    feat: partnership cta section

--- src/components/partnership/cta.tsx
+++ src/components/partnership/cta.tsx
@@ -1,8 +1,6 @@
-import { Button } from "@material-tailwind/react";
 import { useTranslation } from "react-i18next";
-
-import companyLogosImg from "../../assets/home/company-logos.svg";
 import ctaBg from "../../assets/home/cta-bg.svg";
+import envelopeIcon from "../../assets/partnership/envelope-open.svg";
 import "../../i18n";
 
 export default function Cta() {
@@ -10,27 +8,30 @@ export default function Cta() {
 
   return (
     <section
-      className="bg-[#1B0A31] bg-cover bg-center px-6 lg:px-[100px] py-24"
+      className="bg-[#1B0A31] bg-cover bg-center px-6 lg:px-[100px] pt-24 pb-36"
       style={{ backgroundImage: `url(${ctaBg.src})` }}
     >
-      <div className="mx-auto container">
+      <div className="flex flex-col justify-center items-center mx-auto text-white container">
         <div className="flex justify-center items-center">
           <div className="bg-[#281343] px-5 py-3 rounded-3xl font-semibold text-white text-sm lg:text-2xl">
             Otto Gain Smart Solution
           </div>
         </div>
 
-        <h2 className="mt-14 font-semibold text-white text-3xl lg:text-5xl text-center">
-          {t("serviceCta.h2.1")} <br /> {t("serviceCta.h2.2")}
+        <h2 className="mt-14 max-w-[920px] font-semibold text-3xl lg:text-5xl text-center">
+          {t("partnershipCta.h2")}
         </h2>
 
-        <div className="flex justify-center items-center w-full">
-          <Button className="bg-gradient-to-r from-[#C47EFC] to-[#0F17C3] mt-16 font-semibold normal-case">
-            {t("serviceCta.btn")}
-          </Button>
-        </div>
+        <p className="mt-10 max-w-[600px] text-xl text-center">
+          {t("partnershipCta.p1")}
+        </p>
+        <p className="mt-10 max-w-[600px] text-xl text-center">
+          {t("partnershipCta.p2")}
+        </p>
 
-        <img src={companyLogosImg.src} alt="company logos" className="mt-32" />
+        <div className="flex gap-5 mt-5 max-w-[600px] text-xl text-center">
+          <img src={envelopeIcon.src} /> partnership@ottogain.com
+        </div>
       </div>
     </section>
   );
